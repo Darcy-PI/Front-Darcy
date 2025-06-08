fdaadbe24be616f48cda4419fbf4302bd2d19474
= <=>
2025-06-08 11:00:38 -0300

    Adicionando a integração mbiente virtual estudante(GET e POST)

--- darcy/components/Input/Input.jsx
+++ darcy/components/Input/Input.jsx
@@ -1,11 +1,11 @@
 import styles from "./input.module.css";
 
-export default function Input({label,type, id, onChange}){
+export default function Input({label,type, id, onChange, value}){
     return(
         <div className={styles.inputArea}>
 
             <label htmlFor={id} className={styles.label}>{label}</label>
-            <input type={type} id={id} className={styles.input} onChange={onChange} required/>
+            <input type={type} id={id} className={styles.input} value={value} onChange={onChange} required/>
         </div>
     )
 }
