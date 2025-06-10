bc4a61493e08aa0ff3e66dac309738aa50ae6515
Gabriellemllo <gabriellesilvamelo622@gmail.com>
2025-06-10 20:50:55 -0300

    Integração da página de Login e Cadastro

---
+++ darcy/service/loginType/postLoginType.js
@@ -0,0 +1,26 @@
+export default async function postStudentLogin(userName,password,type) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/v1/auth/${type}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        usuario: userName,
+        senha: password,
+      })
+    });
+
+    if (!response.ok) throw new Error('Erro no login!');
+   
+
+    const data = await response.json();
+    console.log(`Login do ${type} bem-sucedido:"`, data);
+    return data; 
+
+  } catch (error) {
+    console.error(`Erro ao fazer login do ${type}:`, error);
+  }
+}
+
+      
---
+++ darcy/service/registerType/postRegisterType.js
@@ -0,0 +1,25 @@
+export default async function postRegisterType(userName,password,fullName, type) {
+  try {
+    const response = await fetch(`http://localhost:8080/api/v1/register/${type}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        usuario: userName,
+        senha: password,
+        nomeCompleto: fullName,
+      })
+    });
+
+    if (!response.ok) throw new Error('Erro no registro!');
+
+    const data = await response.json();
+    console.log(`Registro de ${type} bem-sucedido:`, data);
+    return data;
+
+  } catch (error) {
+    console.error(`Erro ao registrar ${type}:`, error);
+  }
+}
+userName,password,fullName
